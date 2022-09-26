def7d7cbf16c1a2fc78f5ff97119d0b5f91de84a
DellMani <Manikandan_K1@Dell.com>
2022-09-26 16:02:13 +0530

    Console Test

---
+++ C_TEXT_READING/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_TEXT_READING
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Start of Text");
+            // Read the file into a string.
+            string text = System.IO.File.ReadAllText(@"D:\FSD_Training\Projects\Text_file.txt");
+
+            // To Write the file contents to the console screen.
+            System.Console.WriteLine($"{text}", text);
+
+            System.Console.WriteLine("End of Text");
+            System.Console.WriteLine();
+        }
+    }
+}
